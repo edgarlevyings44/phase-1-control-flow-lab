779e9284e77d0bcf03231dc7b11b7dccc2a00307
Levy <edgarlevy81@gmail.com>
2023-07-06 14:40:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(anyValue){
   // Write your code here!
+if (anyValue <= 400){
+    return "This one is on me!";
+}
+else if (anyValue > 2000){
+return "I will gladly take your thirty bucks.";
+}
+else if(anyValue > 2500){
+return "No can do.";
+}
+  }
+function ternaryCheckCity(destination){
+  // Write your code here!
+if (destination === "NYC"){
+  return 'Ok, sounds good.'
+}
+else (destination === 'Pittsburgh');
+  return 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tipPayment){
+  // Write your code here
+  if (tipPayment === "generous") {
+    return "Thank you so much.";
+} else if (tipPayment === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
